fd90934837bc40bdf0fbadae2c11c4cfb765be2f
samk90 <kim.donghy@northeastern.edu>
2021-08-17 12:31:27 -0400

    updated server.js for todo list

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const cookieParser = require('cookie-parser');
 const { connectToDb } = require('./db.js');
 const { installHandler } = require('./api_handler.js');
 const auth = require('./auth.js');
+const todoRoute = require('./todobackend/routes/todo.route.js');
 
 const app = express();
 
 app.use(cookieParser());
 app.use('/auth', auth.routes);
+app.use('/todo', todoRoute);
 
 installHandler(app);
 
